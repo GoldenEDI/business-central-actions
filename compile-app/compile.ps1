f9017e23dec51c3362fd3d6c3d1da0c976a9144a
Alexander Persson <alexander@goldenedi.com>
2020-11-05 15:49:27 +0100

    fix: add fixes for appsourcecop checks

--- compile-app/compile.ps1
+++ compile-app/compile.ps1
@@ -7,11 +7,41 @@ $PASSWORD = $env:INPUT_PASSWORD
 # Get the container name
 $NAME = $env:INPUT_NAME
 
+# Get the current version
+$CURRENT = $env:INPUT_CURRENT
+
 # Build the credentials
 $CREDENTIAL = New-Object pscredential $USERNAME, (ConvertTo-SecureString -String $PASSWORD -AsPlainText -Force)
 
 # Get the workspace
 $WORKSPACE = $env:GITHUB_WORKSPACE
+
+# Check if we should validate against current
+if ($CURRENT -eq "") {
+    # Get the content as JSON
+    $AppSourceCop = Get-Content -Path "$WORKSPACE\AppSourceCop.json" | ConvertFrom-Json
+
+    # Remove the version flag
+    $AppSourceCop.PSObject.Properties.Remove('version')
+
+    # Save the file back as JSON
+    $AppSourceCop | ConvertTo-Json -Depth 100 | Out-File "$WORKSPACE\AppSourceCop.json" -Force
+} else {
+    # Get the content as JSON
+    $AppSourceCop = Get-Content -Path "$WORKSPACE\AppSourceCop.json" | ConvertFrom-Json
+
+    # Check if the property exists
+    if ($AppSourceCop.version -eq $nil) {
+        # Add the current version to the file
+        $AppSourceCop | Add-Member -Name "version" -Value $CURRENT -MemberType NoteProperty
+    } else {
+        # Update the version property
+        $AppSourceCop.version = $CURRENT
+    }
+
+    # Save the file back as JSON
+    $AppSourceCop | ConvertTo-Json -Depth 100 | Out-File "$WORKSPACE\AppSourceCop.json" -Force
+}
 
 # Compile the application
 Compile-AppInBCContainer `
